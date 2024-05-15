cfe5beeeba7ba5a32e3c1c6fc2deff830ae6e6f4
Mihir <Mihir9874@gmail.com>
2024-05-15 16:15:10 +0530

    Developed functionality on frontend component to allow adding new candidates and voters. Added FormsModule to app.module file. Added new API routes in proxy configuration file.

--- VotingApp.Client/src/app/app.component.ts
+++ VotingApp.Client/src/app/app.component.ts
@@ -21,6 +21,10 @@ interface Voter {
 export class AppComponent implements OnInit {
   public candidates: Candidate[] = [];
   public voters: Voter[] = [];
+  public showAddVoterRow: boolean = false;
+  public newVoterName: string = "";
+  public showAddCandidateRow: boolean = false;
+  public newCandidateName: string = "";
 
   constructor(private http: HttpClient) {}
 
@@ -51,12 +55,44 @@ export class AppComponent implements OnInit {
     );
   }
 
-  addVoter() {
-
+  toggleAddVoterRow() {
+    this.showAddVoterRow = !this.showAddVoterRow;
   }
 
-  addCandidate() {
+  saveVoter() {
+    if (this.newVoterName.trim() !== '') {
+      const voterData = { Name: this.newVoterName, type: 1 };
+      this.http.post<any>('/api/voting/addentity', voterData).subscribe(
+        (response) => {
+          this.voters.push({ id: response, name: this.newVoterName, hasVoted: false });
+          this.newVoterName = '';
+          this.showAddVoterRow = false;
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+    }
+  }
 
+  toggleAddCandidateRow() {
+    this.showAddCandidateRow = !this.showAddCandidateRow;
+  }
+
+  saveCandidate() {
+    if (this.newCandidateName.trim() !== '') {
+      const candidateData = { Name: this.newCandidateName, type: 0 };
+      this.http.post<any>('/api/voting/addentity', candidateData).subscribe(
+        (response) => {
+          this.candidates.push({ id: response, name: this.newCandidateName, votes: 0 });
+          this.newCandidateName = '';
+          this.showAddCandidateRow = false;
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+    }
   }
 
   title = 'votingapp.client';
